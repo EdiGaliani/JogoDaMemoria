0f31429a01fdf8571e6bf75e69db45e94862a86f
galiano <edigaliano@gmail.com>
2022-10-20 18:49:08 -0300

    Creating Functions in JS for Cards

---
+++ assets/scripts/script.js
@@ -0,0 +1,41 @@
+const FRONT = "card_front";
+const BACK  = "card_back";
+
+let techs = [
+    'bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquery',
+    'mongo',
+    'node',
+    'react',
+    
+];
+debugger
+createCardsFromTechs(techs);
+function createCardsFromTechs(techs) {
+    let cards = [];
+    for(let tech of techs) {
+        cards.push(createPairFromTech(tech));
+    }
+    return (cards.flatMap(pair=>pair));
+}
+
+function createPairFromTech(tech) {
+    return [{
+        id: creatIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }, {
+        id: creatIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }]
+}
+
+function creatIdWithTech(tech) {
+    return parseInt(Math.random() * 1000);
+}
